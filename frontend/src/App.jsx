f6c779c92d584fc2eac1d17f1eb8e48696c6edcc
sai kishen srinivasa babu <ssaikishen@gmail.com>
2026-02-28 20:12:35 -0500

    test push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,16 @@ import AboutPage from "./AboutPage";
 import MenuPage from "./MenuPage";
 import ContactPage from "./ContactPage";
 import ReservationModal from "./components/ReservationModal";
-
+import { useEffect } from "react";
+import axios from "axios"
 function App() {
+
+  useEffect(()=>{
+    axios.post("/api/products").then(x=>{
+      console.log("this is the res", x)
+    })
+
+  },[])
   return (
     <Router>
       <TopNavbar />
